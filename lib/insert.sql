71e7128e0d7b589b55adb0396e0f86478dcf1d72
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2024-06-13 22:24:04 -0700

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 3, "M", "Black", "Loud", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "Brown", "Nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 50, "M", "Brown", "Calm", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "F", "White", "Low", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 55, "M", "Black", "Low", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "F", "White", "Nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 15, "M", "Brown", "Mild", 0);
